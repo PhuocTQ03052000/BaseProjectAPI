1b6457c4fbcc0e97d3ba60dc7efe16fd43950474
PhuocTQ03052000 <phuoctq03052000@gmail.com>
2023-12-28 17:44:55 +0700

    first commit

---
+++ Data/BookStoreDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BaseProjectAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BaseProjectAPI.Data
+{
+    public class BookStoreDbContext: DbContext
+    {
+        public BookStoreDbContext(DbContextOptions<BookStoreDbContext> options) : base(options) { }
+
+        #region DbSet
+        public DbSet<Book>? Books { get; set; }
+        #endregion DbSet
+    }
+}
